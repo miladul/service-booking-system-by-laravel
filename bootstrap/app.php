0223ab075b9be3a711f2da6863ad9ffc5ad5c345
Miladul <imiladul@gmail.com>
2025-07-29 01:31:31 +0600

    exception handle for api error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,29 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+
+        $exceptions->renderable(function (Throwable $exception, Request $request) {
+            //NotFoundHttpException
+
+            if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $exception->getMessage(),
+
+                ], 404);
+            }
+
+            //Default error exception
+            if ($request->ajax() || $request->wantsJson()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $exception->getMessage(),
+                ], 500);
+            }
+
+            // Let Laravel handle it for non-JSON requests
+            return null;
+
+        });
+
     })->create();
