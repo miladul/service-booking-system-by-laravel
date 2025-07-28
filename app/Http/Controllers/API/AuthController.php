88a074612fc3758a48b007396fa8b8af954c2b88
Miladul <imiladul@gmail.com>
2025-07-28 22:12:10 +0600

    register customer

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'is_admin' => false,
+        ]);
+
+        $token = $user->createToken('CustomerToken')->accessToken;
+
+        return response()->json(['token' => $token, 'user' => $user]);
+    }
+
+
 }
+
