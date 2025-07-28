6bd6298e8759c52bbb48e65ac413f0fadea8b587
Miladul <imiladul@gmail.com>
2025-07-28 22:27:10 +0600

    Seeder create for admin user & some services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @method static factory()
+ */
 class Service extends Model
 {
+    use HasFactory;
     protected $table = 'services';
     protected $guarded = ['id'];
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Service;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
         ]);
+
+        \App\Models\Service::factory()->count(5)->create();
     }
 }
